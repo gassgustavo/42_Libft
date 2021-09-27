beb46ccd9a3349cf6aa2b901d79b79a02f617b56
gmoraes- <gustmgass@gmail.com>
2021-09-27 19:02:19 -0300

    Fixed memset

--- ft_memset.c
+++ ft_memset.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/27 16:44:46 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/09/27 16:55:02 by gmoraes-         ###   ########.fr       */
+/*   Created: 2021/09/27 18:53:39 by gmoraes-          #+#    #+#             */
+/*   Updated: 2021/09/27 18:56:13 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void    *ft_memset(void *str, int byte, size_t n)
 {
-    int     i;
     char    *temp;
+    int     i;
 
-    i = 0;
     temp = (char *)str;
+    i = 0;
     while (i < n)
     {
-        temp = (unsigned char)byte;
+        temp[i] = (unsigned char)byte;
         i++;
     }
     return (str);
