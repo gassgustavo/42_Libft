f5a939dff7c9fd04e6d423f956358b4ec6f9d6cb
gmoraes- <gustmgass@gmail.com>
2021-09-28 12:26:27 -0300

    just testing an idea

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 10:10:12 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/09/28 12:10:14 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/09/28 12:25:50 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,12 @@
 char	*ft_strjoin(char const *str1, char const *str2)
 {
 	char	*n_str;
-	int		i;
-	int		j;
 
 	if (!str1 || !str2)
 		return (0);
 	n_str = malloc((ft_strlen(str1) + ft_strlen(str2) + 1) * sizeof(char));
 	if (!n_str)
 		return (0);
-	i = 0;
-	j = 0;
-	while (str1[i] != '\0')
-	{
-		n_str[j] = str1[i];
-		j++;
-		i++;
-	}
-	i = 0;
-	while (str2[i] != '\0')
-	{
-		n_str[j] = str2[i];
-		j++;
-		i++;
-	}
-	n_str[j] = '\0';
+	n_str = ft_strlcat(str1, str2, n_str);
 	return (n_str);
 }
