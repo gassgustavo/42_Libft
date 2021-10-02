2c3426168add5e816394552d836ec4fcc0d4cb6f
gmoraes- <gustmgass@gmail.com>
2021-10-02 16:55:07 -0300

    itoa day 2: 1st try

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 23:12:27 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/01 18:33:10 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/02 16:54:19 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,24 @@ char	*ft_itoa(int n)
 {
 	size_t	len;
 	char	*str;
+	int		n_temp;
 
-	len = nlen(n);
+	n_temp = n;
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len = nlen(n_temp);
 	str = malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (0);
 	if (n < 0)
+	{
 		str[0] = '-';
+		n_temp *= -1;
+	}
 	while (len > 0)
 	{
-		str[len] = ntochr(n);
+		str[len] = ntochr(n_temp);
+		n_temp /= 10;
 		len--;
 	}
 }
