c55425ecad0c891d03aeaa33265f11275c7f105a
gmoraes- <gustmgass@gmail.com>
2021-10-03 20:16:11 -0300

    testing strrchr (9)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,24 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 18:00:41 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 20:15:04 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 20:16:06 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include "libft.h"
-
 char	*ft_strrchr(const char *str, int c)
 {
-	int		leng;
+	int	len;
 
-	leng = ft_strlen(str);
-	while (leng >= 0)
+	if (str[0] == '\0')
+		return (0);
+	len = ft_strlen(str);
+	while (len >= 0)
 	{
-		if (str[leng] == (char)c)
-			return (&str[leng]);
-		leng--;
+		if (str[len] == (char)c)
+			return ((char *)&str[len]);
+		len--;
 	}
 	return (0);
 }
