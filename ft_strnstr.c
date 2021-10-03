8aa2b2d2b66d791707705e4c5edc77181b3906b2
gmoraes- <gustmgass@gmail.com>
2021-10-03 19:40:37 -0300

    testing strnstr (15)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 13:12:34 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 19:37:35 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 19:40:32 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,24 +29,6 @@
 	return (0);
 }*/
 
-char	*ft_strnstr(const char *str, const char *substr, size_t len)
-{
-	size_t	i;
-	size_t	substr_len;
-
-	i = 0;
-	if (substr[0] == '\0')
-		return ((char *)str);
-	substr_len = ft_strlen(substr);
-	while ((char *)&str[i] && len-- >= substr_len)
-	{
-		if (ft_strncmp((char *)&str[i], (char *)substr, substr_len) == 0)
-			return ((char *)&str[i]);
-		i++;
-	}
-	return (0);
-}
-
 /*char	*ft_strnstr(const char *str, const char *substr, size_t len)
 {
 	size_t	i;
@@ -56,7 +38,26 @@ char	*ft_strnstr(const char *str, const char *substr, size_t len)
 	if (substr[0] == '\0')
 		return ((char *)str);
 	substr_len = ft_strlen(substr);
-	while (str[i] != '\0' && len - 1 > 0)
+	while ((char *)&str[i] && len >= substr_len)
+	{
+		if (ft_strncmp((char *)&str[i], (char *)substr, substr_len) == 0)
+			return ((char *)&str[i]);
+		i++;
+		len--;
+	}
+	return (0);
+}*/
+
+char	*ft_strnstr(const char *str, const char *substr, size_t len)
+{
+	size_t	i;
+	size_t	substr_len;
+
+	i = 0;
+	if (substr[0] == '\0')
+		return ((char *)str);
+	substr_len = ft_strlen(substr);
+	while (str[i] != '\0' && len >= substr_len)
 	{
 		if (ft_strncmp(&str[i], substr, substr_len) == 0)
 			return ((char *)&str[i]);
@@ -65,4 +66,3 @@ char	*ft_strnstr(const char *str, const char *substr, size_t len)
 	}
 	return (0);
 }
-*/
