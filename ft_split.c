9a8096f33cee71bd857a88cb576deb18e0ee96f1
gmoraes- <gustmgass@gmail.com>
2021-10-03 15:20:34 -0300

    fix ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/03 03:45:31 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 15:16:06 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 15:20:04 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ static char	**get_words(char const *str, char c, char **list)
 {
 	unsigned int	i;
 	unsigned int	word_len;
-	unsigned int	k;
+	unsigned int	w;
 
 	i = 0;
-	k = 0;
+	w = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] != c)
@@ -53,25 +53,11 @@ static char	**get_words(char const *str, char c, char **list)
 				word_len++;
 				i++;
 			}
-			list[k] = (char *)ft_calloc(word_len + 1, sizeof(char));
-			ft_strlcpy(list[k], &str[i - word_len], word_len + 1);
-			k++;
+			list[w] = (char *)ft_calloc(word_len + 1, sizeof(char));
+			ft_strlcpy(list[w], &str[i - word_len], word_len + 1);
+			w++;
 		}
 		i++;
-		/*if (str[i] == c)
-			i++;
-		else
-		{
-			j = 0;
-			while (str[i] != c && str[i])
-			{
-				i++;
-				j++;
-			}
-			list[k] = (char *)ft_calloc(j + 1, sizeof(char));
-			ft_strlcpy(list[k], &str[i - j], j + 1);
-			k++;
-		}*/
 	}
 	return (list);
 }
@@ -83,7 +69,7 @@ char	**ft_split(char const *str, char c)
 	if (!str)
 		return (0);
 	list = (char **)ft_calloc(count_words(str, c) + 1, sizeof(char *));
-	if (!list)
-		return (0);
-	return (get_words(str, c, list));
+	if (list)
+		return (get_words(str, c, list));
+	return (0);
 }
