c8cd249492dca18f33ca7a8ad630437f31071dd9
gmoraes- <gustmgass@gmail.com>
2021-10-03 17:48:04 -0300

    testing atoi (5)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,25 +6,13 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 15:43:32 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 17:44:52 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 17:47:59 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 #include "libft.h"
-
-static int	ft_isspace(char c)
-{
-	if ((c == '\n')
-		|| (c == '\t')
-		|| (c == '\v')
-		|| (c == '\f')
-		|| (c == '\r')
-		|| (c == ' '))
-		return (1);
-	return (0);
-}
 
 int	ft_atoi(const char *s)
 {
@@ -37,7 +25,7 @@ int	ft_atoi(const char *s)
 	signal = 1;
 	response = 0;
 	str = (char *)s;
-	while (ft_isspace(str[i]))
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
