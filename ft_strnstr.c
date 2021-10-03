d9c65df964e97e3529c038040625f64513c25209
gmoraes- <gustmgass@gmail.com>
2021-10-03 19:29:04 -0300

    testing strnstr (8)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,24 +6,25 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 13:12:34 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 19:27:15 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 19:28:59 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t len)
+char	*ft_strnstr(const char *str, const char *substr, size_t len)
 {
-	size_t len2;
+	size_t	i;
+	size_t	substr_len;
 
-	if (*s2 == '\0')
-		return ((char *)s1);
-	len2 = ft_strlen(s2);
-	while (*s1 != '\0' && len-- >= len2)
+	if (substr[0] == '\0')
+		return ((char *)str);
+	substr_len = ft_strlen(substr);
+	while (*str != '\0' && len-- >= substr_len)
 	{
-		if (*s1 == *s2 && ft_strncmp(s1, s2, len2) == 0)
-			return ((char *)s1);
-		s1++;
+		if (*str == *substr && ft_strncmp(str, substr, substr_len) == 0)
+			return ((char *)str);
+		str++;
 	}
-	return (NULL);
+	return (0);
 }
