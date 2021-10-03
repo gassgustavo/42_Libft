b1bbbaef1c87dfad823c5a785ceff4574ab3b414
gmoraes- <gustmgass@gmail.com>
2021-10-03 20:58:07 -0300

    testing strtrim (8)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,22 +6,39 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 14:32:19 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 20:54:53 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 20:58:03 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+/*char	*ft_strtrim(char const *str, char const *set)
 {
-	size_t	len;
+	size_t	start;
+	size_t	end;
 
-	if (!s1 || !set)
+	if (!str || !set)
 		return (0);
-	while (*s1 && ft_strchr(set, *s1))
-		s1++;
-	len = ft_strlen(s1);
-	while (len && ft_strchr(set, s1[len]))
-		len--;
-	return (ft_substr(s1, 0, len + 1));
+	start = 0;
+	while (str[start] != '\0' && strchr(set, str[start]))
+		start++;
+	end = ft_strlen(str);
+	while (end != 0 && strchr(set, str[end]))
+		end--;
+	return (ft_substr(str, start, end + 1));
+}*/
+
+char	*ft_strtrim(char const *str, char const *set)
+{
+	int		start;
+	size_t	end;
+
+	if (!str || !set)
+		return (0);
+	while (str[start] && ft_strchr(set, str[start]))
+		start++;
+	end = ft_strlen(str);
+	while (end && ft_strchr(set, str[end]))
+		end--;
+	return (ft_substr(str, start, end + 1));
 }
