24e05fe19947856254de3797b4d80d70707ea0a8
gmoraes- <gustmgass@gmail.com>
2021-10-03 20:21:37 -0300

    strrchr done

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 17:18:06 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/09/28 10:34:18 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 20:21:22 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@ char	*ft_strchr(const char *str, int c)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && str[i] != c)
+	while (str[i] != '\0')
 	{
+		if (str[i] == (unsigned char)c)
+			return ((char *)&str[i]);
 		i++;
-	}
-	if (str[i] == (char)c)
-	{
-		return ((char *)&str[i]);
 	}
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 18:00:41 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 20:18:02 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 20:18:34 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,3 @@ char	*ft_strrchr(const char *str, int c)
 	}
 	return (0);
 }
-
-/*char	*ft_strrchr(const char *str, int c)
-{
-	int	len;
-
-	len = ft_strlen(str);
-	while (len >= 0)
-	{
-		if (str[len] == (char)c)
-			return (&str[len]);
-		len--;
-	}
-	return (0);
-}*/
