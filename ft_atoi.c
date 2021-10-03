79db89c70d9ce64ba4201da538756af027508db3
gmoraes- <gustmgass@gmail.com>
2021-10-03 17:54:36 -0300

    testing atoi (8)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 15:43:32 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 17:50:06 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 17:54:24 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,27 @@
 
 #include "libft.h"
 
-int	ft_atoi(const char *s)
+int	ft_atoi(const char *str)
 {
-	int		i;
-	int		signal;
-	int		response;
-	char	*str;
+	int	i;
+	int	num;
+	int	sign;
 
 	i = 0;
-	signal = 1;
-	response = 0;
-	str = (char *)s;
-	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+	num = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			signal *= -1;
+			num *= -1;
 		i++;
 	}
 	while (ft_isdigit(str[i]))
 	{
-		response = (str[i] - '0') + (response * 10);
+		num = (num * 10) + (str[i] - 48);
 		i++;
 	}
-	return (response * signal);
+	return (num * sign);
 }
