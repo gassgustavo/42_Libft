cb780ccdaa25ec5267acdcf94cee0b683cf4164f
gmoraes- <gustmgass@gmail.com>
2021-10-03 20:54:57 -0300

    testing strtrim (7)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,22 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 14:32:19 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 20:52:21 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 20:54:53 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *str, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	start;
-	size_t	end;
+	size_t	len;
 
-	if (!str || !set)
+	if (!s1 || !set)
 		return (0);
-	start = 0;
-	while (str[start] != '\0' && strchr(set, str[start]))
-		start++;
-	end = ft_strlen(str);
-	while (end != 0 && strchr(set, str[end]))
-		end--;
-	return (ft_substr(str, start, end + 1));
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (len && ft_strchr(set, s1[len]))
+		len--;
+	return (ft_substr(s1, 0, len + 1));
 }
