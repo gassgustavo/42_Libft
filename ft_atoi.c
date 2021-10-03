ab559aa4759e4d49b949acf19b55073fe83e963e
gmoraes- <gustmgass@gmail.com>
2021-10-03 17:58:22 -0300

    testing atoi (10)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 15:43:32 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 17:55:54 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 17:58:16 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@
 int	ft_atoi(const char *str)
 {
 	int		i;
-	int		signal;
-	int		response;
+	int		sign;
+	int		num;
 
 	i = 0;
-	signal = 1;
-	response = 0;
+	sign = 1;
+	num = 0;
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			signal *= -1;
+			sign *= -1;
 		i++;
 	}
 	while (ft_isdigit(str[i]))
 	{
-		response = (str[i] - '0') + (response * 10);
+		num = (num * 10) + (str[i] - 48);
 		i++;
 	}
-	return (response * signal);
+	return (num * sign);
 }
