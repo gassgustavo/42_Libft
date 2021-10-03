e57788e35e167b5cdb6a59a73a618eb7256cd38b
gmoraes- <gustmgass@gmail.com>
2021-10-03 19:26:19 -0300

    testing strnstr (6)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,49 +6,25 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 13:12:34 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 19:16:25 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 19:26:14 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*char	*ft_strnstr(const char *str, const char *substr, size_t len)
-{
-	size_t	i;
-	size_t	j;
-	size_t	substr_len;
-
-	i = 0;
-	j = 0;
-	if (substr[0] == '\0')
-		return ((char *)str);
-	substr_len = ft_strlen(substr);
-	while ((char *)&str[i] && (j < len))
-	{
-		if ((ft_strncmp((char *)&str[i], (char *)substr, substr_len) == 0)
-			&& (j + substr_len <= len))
-			return ((char *)&str[i]);
-		i++;
-		j++;
-	}
-	return (0);
-}*/
 
 char	*ft_strnstr(const char *str, const char *substr, size_t len)
 {
 	size_t	i;
 	size_t	substr_len;
 
-	i = 0;
 	if (substr[0] == '\0')
 		return ((char *)str);
 	substr_len = ft_strlen(substr);
-	while ((char *)str[i] != '\0' && len - 1 > 0)
+	while ((char *)&str[i] != '\0' && len-- >= substr_len)
 	{
-		if (ft_strncmp((char *)&str[i], substr, substr_len) == 0)
-			return ((char *)&str[i]);
+		if ((char *)&str[i] == *substr && ft_strncmp(str, substr, substr_len) == 0)
+			return ((char *)str);
 		i++;
-		len--;
 	}
 	return (0);
 }
