ce484b3b4822b2fc28e59310709c463fedb88e42
gmoraes- <gustmgass@gmail.com>
2021-10-03 22:20:11 -0300

    testing strtrim (41)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,30 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 14:32:19 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 22:09:49 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 22:19:57 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strtrim(char const *str, char const *set)
+{
+	int	start;
+	int	end;
+
+	if (!str || !set)
+		return (0);
+	start = 0;
+	while (str[start] == set)
+		start++;
+	end = ft_strlen(str);
+	while (str[end - 1] == set)
+	{
+		end--;
+	}
+	end = end + 1;
+	return (ft_substr(str, start, end));
+}
 
 char	*ft_strtrim(char const *str, char const *set)
 {
@@ -20,11 +39,10 @@ char	*ft_strtrim(char const *str, char const *set)
 	if (!str || !set)
 		return (0);
 	start = 0;
-	while (str && ft_strchr(set, str[start]))
+	while (str[start] == set)
 		start++;
 	end = ft_strlen(str);
-	while (end && ft_strchr(set, str[end]))
+	while (end == set)
 		end--;
-	return (ft_substr(str, str[start], end + 1));
+	return (ft_substr(str, start, (end - start) + 1));
 }
-
