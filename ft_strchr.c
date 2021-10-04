2bed21da59e112c4637c9b1a639c655da286b34e
gmoraes- <gustmgass@gmail.com>
2021-10-03 23:05:33 -0300

    testing strchr (1)

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 17:18:06 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 20:24:06 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 23:05:18 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strchr(const char *str, int c)
 	i = 0;
 	while (str[i] != '\0' && str[i] != c)
 		i++;
-	if (str[i] == (char)c)
+	if (str[i] == (unsigned char)c)
 		return ((char *)&str[i]);
 	return (0);
 }
