0544edc3128de69454ec7034ecb72e479d04593a
gmoraes- <gustmgass@gmail.com>
2021-10-04 02:09:39 -0300

    testing itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 01:02:25 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/04 02:08:15 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/04 02:09:35 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,14 @@ static char	ntochar(int n)
 char	*ft_itoa(int n)
 {
 	int		len;
-	int		n_temp;
 	char	*str;
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
 	len = nlen(n);
-	n_temp = n;
 	if (n < 0)
 	{
-		n_temp *= -1;
+		n *= -1;
 		len = len + 1;
 	}
 	str = (char *)ft_calloc((len + 1), sizeof(char));
@@ -53,8 +51,8 @@ char	*ft_itoa(int n)
 		return (0);
 	while (len >= 1)
 	{
-		str[len - 1] = ntochar(n_temp);
-		n_temp = n_temp / 10;
+		str[len - 1] = ntochar(n);
+		n = n / 10;
 		len--;
 	}
 	if (n < 0)
