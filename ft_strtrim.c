049a8896740bc464b244fd6ea1174f4d429aa7a5
gmoraes- <gustmgass@gmail.com>
2021-10-03 21:28:33 -0300

    testing strtrim (19)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 14:32:19 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 21:26:17 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 21:28:27 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 	start = 0;
 	while (str[start] != '\0' && strchr(set, str[start]))
 		start++;
-	end = ft_strlen(str);
+	end = ft_strend(str);
 	while (end != 0 && strchr(set, str[end]))
 		end--;
 	return (ft_substr(str, start, end + 1));
@@ -30,18 +30,14 @@
 
 char	*ft_strtrim(char const *str, char const *set)
 {
-	int		i;
-	size_t	start;
 	size_t	end;
 
-	i = 0;
-	start = 0;
 	if (!str || !set)
 		return (0);
-	while (str[start] && ft_strchr(set, str))
-		start++;
+	while (*str && ft_strchr(set, *str))
+		str++;
 	end = ft_strlen(str);
 	while (end && ft_strchr(set, str[end]))
 		end--;
-	return (ft_substr(str, start, end + 1));
+	return (ft_substr(str, 0, end + 1));
 }
