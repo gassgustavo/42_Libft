48ca5363cdf1c78a66621096e1236d10511abcbf
gmoraes- <gustmgass@gmail.com>
2021-10-03 21:01:12 -0300

    testing strtrim (10)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 14:32:19 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/03 21:00:26 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 21:01:08 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ char	*ft_strtrim(char const *str, char const *set)
 	int		start;
 	size_t	end;
 
-	start = 1;
+	start = 0;
 	if (!str || !set)
 		return (0);
 	while (str[start] && ft_strchr(set, str[start]))
