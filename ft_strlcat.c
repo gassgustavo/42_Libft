4804a744c73890e1a357913481200ea960c72c9b
gmoraes- <gustmgass@gmail.com>
2021-10-03 23:22:36 -0300

    testing strlcat (1)

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 01:19:05 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/09/28 11:47:05 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/03 23:22:24 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,21 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	destlen;
-	size_t	srclen;
-	size_t	n_str;
-	size_t	i;
+	size_t i;
+	size_t j;
 
-	destlen = ft_strlen(dest);
-	srclen = ft_strlen(src);
-	n_str = destlen + srclen;
-	i = 0;
-	if (destlen > size)
-		return (srclen + size);
-	while (src[i] != '\0' && destlen < (size - 1))
+	i = ft_strlen(dest);
+	j = 0;
+	if (i < size)
 	{
-		dest[destlen] = src[i];
-		destlen++;
-		i++;
+		while (i < size - 1 && src[j] != '\0')
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
+		dest[i] = '\0';
+		return (ft_strlen(dest) + ft_strlen(src));
 	}
-	dest[destlen] = '\0';
-	return (n_str);
+	return (size + ft_strlen(src));
 }
