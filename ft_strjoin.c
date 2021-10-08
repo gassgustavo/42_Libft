dd7def4d39bfe7775fc259aeb5d5d3a65d8a8ac0
gmoraes- <gustmgass@gmail.com>
2021-10-08 11:20:20 -0300

    previous version

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,13 @@
 /*   By: gmoraes- <gmoraes-l@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 10:10:12 by gmoraes-          #+#    #+#             */
-/*   Updated: 2021/10/08 11:17:00 by gmoraes-         ###   ########.fr       */
+/*   Updated: 2021/10/08 11:18:29 by gmoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*char	*ft_strjoin(char const *str1, char const *str2)
+char	*ft_strjoin(char const *str1, char const *str2)
 {
 	char	*new_str;
 	int		i;
@@ -39,22 +39,4 @@
 	}
 	new_str[j] = '\0';
 	return (new_str);
-}*/
-
-char	*ft_strjoin(char const *str1, char const *str2)
-{
-	char	*new_str;
-	size_t	str1_len;
-	size_t	str2_len;
-
-	if (!str1 || !str2)
-		return (0);
-	str1_len = ft_strlen(str1);
-	str2_len = ft_strlen(str2);
-	new_str = calloc((str1_len + str2_len + 1), sizeof(char));
-	if (!new_str)
-		return (0);
-	ft_strlcpy(new_str, str1, str1_len);
-	ft_strlcat(new_str, str2, str1_len);
-		return (new_str);
 }
